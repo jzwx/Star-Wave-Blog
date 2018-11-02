dfeff8a9961d4ee7d001b0591c3995dadb9389a3
jzwx <2058631403@qq.com>
2018-11-02 14:55:46 +0800

    wangxin

--- src/main/resources/static/js/userspace/main.js
+++ src/main/resources/static/js/userspace/main.js
@@ -52,7 +52,7 @@ $(function() {
  	    formData.append("file",convertBase64UrlToBlob(base64Codes));  //append函数的第一个参数是后台获取数据的参数名,和html标签的input的name属性功能相同  
 	    
  	    $.ajax({
-		    url: 'http://172.18.19.43:8081/upload',
+		    url: 'http://47.107.50.13:8081/upload',
 		    type: 'POST',
 		    cache: false,
 		    data: formData,
